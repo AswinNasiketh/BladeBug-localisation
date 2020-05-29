54bbec8bc3f38ba82a2652031d550e4b511e9d8b
Nosilil <38347069+Nosilil@users.noreply.github.com>
2020-05-29 13:14:47 +0100

    changed data type to more generic matrix_t

--- trmultilat.c
+++ trmultilat.c
@@ -3,39 +3,39 @@
 #include "../LinAlg/linalg.h" //needs to be updated when makefile is created
 
 #define V_S 343.0 //speed of sound in air in m/s - update later
-
+// matrix_t is the data type for elements in the matrix structure. Can be either double or float
 typedef struct 
 {
-    float x, y ,z;
+    matrix_t x, y ,z;
 } Point;
 
-float calculate_distancesq_from_origin(Point point){
-    return powf(point.x, 2) + powf(point.y, 2) + powf(point.z, 2);
+matrix_t calculate_distancesq_from_origin(Point point){
+    return (point.x) * (point.x) + (point.y) * (point.y) + (point.z) * (point.z);
 }
 
-SquareMatrix form_matrix_system( Point* receiver, float* ri){ // ri array being distance between rxi and bug
-    float** matrix_els = malloc(3 * sizeof(float*));
+SquareMatrix form_matrix_system( Point* receiver, matrix_t* ri){ // ri array being distance between rxi and bug
+    matrix_t** matrix_els = malloc(3 * sizeof(matrix_t*));
 
-    float* Ri_sq = malloc(4 * sizeof(float)); //Ri_sq being an array of distance between rxi and origin squared
+    matrix_t* Ri_sq = malloc(4 * sizeof(matrix_t)); //Ri_sq being an array of distance between rxi and origin squared
     
     Ri_sq[0] = 0;
     Ri_sq[1] = calculate_distancesq_from_origin(receiver[1]);
     Ri_sq[2] = calculate_distancesq_from_origin(receiver[2]);
     Ri_sq[3] = calculate_distancesq_from_origin(receiver[3]);
-    float r0_sq = powf(r[0], 2);
+    matrix_t r0_sq = r[0] * r[0];
 
-    float B* = malloc(4 * sizeof(float));
+    matrix_t B* = malloc(4 * sizeof(matrix_t));
 
     // 2x(-xi) + 2y(-yi) + 2z(-zi) = -Ri^2 + ri^2 - r0^2
     for(int i = 0; i < 3; i++){
-        matrix_els[i] = malloc(3 * sizeof(float));
+        matrix_els[i] = malloc(3 * sizeof(matrix_t));
 
         //the equation start with point 1 instead of point 0
         matrix_els[i][0] = -2*receiver[i+1].x; //x
         matrix_els[i][1] = -2*receiver[i+1].y; //y
         matrix_els[i][2] = -2*receiver[i+1].z; //z
 
-        B[i] = -Ri_sq[i+1] + pow(ri[i+1],2) - r0_sq; 
+        B[i] = -Ri_sq[i+1] + (ri[i+1] * ri[i+1]) - r0_sq; 
     }
 
     SquareMatrix matrix_system = init_square_matrix(3, matrix_els);
