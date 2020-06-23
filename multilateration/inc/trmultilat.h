42c13393daa128ef0c8c2ef1c616385dc0f2577b
Aswin Vivekanandan <aswinvivekanandan@gmail.com>
2020-06-24 00:18:15 +0100

    remove distance squared function from trmultilat.h

--- multilateration/inc/trmultilat.h
+++ multilateration/inc/trmultilat.h
@@ -4,7 +4,6 @@
 #include "linalg.h"
 #include "geometry.h"
 
-matrix_t calculate_distancesq_from_origin(Point point);
-SquareMatrix form_matrix_system( Point* receiver, matrix_t* ri, matrix_t* B);
+SquareMatrix form_matrix_system(Point* receiver, matrix_t* ri, matrix_t* B);
 
 #endif
